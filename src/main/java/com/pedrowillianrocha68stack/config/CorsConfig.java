edf72cdb5308a089a612021fcd27796fc431c49b
pedrowillian <pedrowillianrocha68@gmail.com>
2026-05-30 22:52:06 -0300

    fix: correção de erro da classe CorsConfig

--- src/main/java/com/pedrowillianrocha68stack/config/CorsConfig.java
+++ src/main/java/com/pedrowillianrocha68stack/config/CorsConfig.java
@@ -3,13 +3,11 @@ package com.pedrowillianrocha68stack.config;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer{
     @Override
-    public void addCorsMapping(CorsRegistry registry){
+    public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE"); 
